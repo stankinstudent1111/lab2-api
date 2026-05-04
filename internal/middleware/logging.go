143bc75931bf2bf4e9e2c96146ad08bb23773929
Mikhail Boldin <mishboldi@yandex.ru>
2026-05-04 16:33:53 +0300

    add request method in log

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -10,6 +10,9 @@ func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		next.ServeHTTP(w, r)
 
-		log.Printf("Запрос пришел")
+		log.Printf(
+			"[%s] Запрос пришел",
+			r.Method,
+		)
 	})
 }
